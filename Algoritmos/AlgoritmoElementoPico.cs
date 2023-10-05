205c6ce651e16c3732c9533243f118626cc33aff
Lucas Dirani <lruizd@protonmail.com>
2023-10-05 08:36:06 -0300

    Adicionar arquivos de projeto.

---
+++ Algoritmos/AlgoritmoElementoPico.cs
@@ -0,0 +1,44 @@
+﻿namespace EncontrarElementoDePico.App.Algoritmos
+{
+    public static class AlgoritmoElementoPico
+    {
+        public static int EncontrarElementoDePicoEmComplexidadeLinearDeTempo(int[] arr, int n)
+        {
+            if (arr[0] > arr[1]) return 0;
+            if (arr[n - 1] > arr[n - 2]) return n - 1;
+            for (int i = 1; i < n - 1; i++)
+            {
+                if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int EncontrarElementoDePicoEmComplexidadeLogaritmicaDeTempo(int[] arr, int n)
+        {
+            int esquerda = 0;
+            int direita = n - 1;
+            int metade;
+            while (esquerda <= direita)
+            {
+                metade = (esquerda + direita) >> 1;
+                if ((metade == 0 || arr[metade - 1] <= arr[metade])
+                    && (metade == n - 1 || arr[metade + 1] <= arr[metade]))
+                {
+                    return metade;
+                }
+                else if (metade > 0 && arr[metade - 1] > arr[metade])
+                {
+                    direita = metade - 1;
+                }
+                else
+                {
+                    esquerda = metade + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using EncontrarElementoDePico.App.Performance;
+
+_ = BenchmarkRunner.Run<TestePerformanceAlgoritmoElementoPico>();
